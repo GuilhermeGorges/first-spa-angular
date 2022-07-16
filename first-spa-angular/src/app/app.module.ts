5da424de8784405ef518aff62ea65ff8e9163e9d
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-07-16 19:57:17 -0300

    Routes & Modules

--- first-spa-angular/src/app/app.module.ts
+++ first-spa-angular/src/app/app.module.ts
@@ -1,30 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
 import { AlunosComponent } from './alunos/alunos.component';
 import { ProfessoresComponent } from './professores/professores.component';
-import { ModelComponent } from './model/model.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AlunosComponent,
-    ProfessoresComponent,
-    ModelComponent
+    ProfessoresComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
