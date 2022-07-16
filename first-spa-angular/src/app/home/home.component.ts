f1436fd3bb91a944cc6741ce0a22f75e6dea5735
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-07-16 19:51:07 -0300

    HOME

--- first-spa-angular/src/app/home/home.component.ts
+++ first-spa-angular/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,7 +10,32 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  value: number;
+  showTable: boolean = false;
+
+  studentList: student[] = [
+    { name: 'Guilherme', age: '30', email: 'guilherme@gmail.com', course: 'Computer Cience'},
+    { name: 'Guilherme', age: '30', email: 'guilherme@gmail.com', course: 'Computer Cience'},
+    { name: 'Guilherme', age: '30', email: 'guilherme@gmail.com', course: 'Computer Cience'},
+    { name: 'Guilherme', age: '30', email: 'guilherme@gmail.com', course: 'Computer Cience'}
+  ];
+
+  displayedColums: string[] = ['Nome', 'Idade', 'Email', 'Curso'];
+
   ngOnInit(): void {
+    this.value = 5;
+  }
+
+  changeValue(){
+    this.value++;
+  }
+
+  restartValue(){
+    this.value = 0;
+  }
+
+  getTable(){
+    this.showTable = true;
   }
 
 }
